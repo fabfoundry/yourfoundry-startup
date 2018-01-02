6ad6967c5804ac43d8e558a7d92cd4aa4eed5adf
Peter <ppapadim12@gmail.com>
2018-01-01 20:20:50 -0500

    invision mock init buildout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import LoginForm from '../components/Home/loginForm';
 import CreateUserForm from '../components/Home/createUserForm';
 import LogoutButton from '../components/Home/logoutButton';
+import Navbar from '../components/navbar';
 import '../stylesheets/home.css';
 
 class Home extends Component{
-  
+
   render(){
     return(
-      <div className="container-fluid page-container">
-        <div className="row" id="auth-forms-container">
-          <div className="col-md-5 auth-forms">
-            <LoginForm/>
+    <div >
+      <div className="container-fluid" id="home-page">
+        <div className="row">
+          <Navbar />
+        </div>
+        <div className="row" id="home-content">
+          <div className="col-md-2">
           </div>
-          <div className="col-md-2 center-OR">
-              <p id="login-OR-create">OR</p>
+          <div className="col-md-8" id="home-content-container">
           </div>
-          <div className="col-md-5 auth-forms">
-            <CreateUserForm/>
+          <div className="col-md-2">
           </div>
         </div>
       </div>
+    </div>
     )
   }
 }
