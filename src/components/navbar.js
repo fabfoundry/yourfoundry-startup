c8ce68008d9d0ab6c9f6bf4391161c907f224f47
Peter <ppapadim12@gmail.com>
2018-01-02 19:56:02 -0500

    landing page style complete

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <a className="navbar-brand navbar-logo" href="#">YourFoundry</a>
+          <a className="navbar-brand navbar-logo" href="/">YourFoundry</a>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
             </ul>
