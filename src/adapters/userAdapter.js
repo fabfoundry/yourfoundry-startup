22bf98d91fb32f9162f97fb14e7fc0d62c93767c
Peter <ppapadim12@gmail.com>
2018-01-11 18:17:42 -0500

    updated cloudfront endpoints

--- src/adapters/userAdapter.js
+++ src/adapters/userAdapter.js
@@ -2,7 +2,7 @@ class UserAdapter {
 
   static create(credentials) {
 
-    const request = new Request("https://d4ue5pwgatcq3.cloudfront.net/api/v1/users", {
+    const request = new Request("http://d4ue5pwgatcq3.cloudfront.net/api/v1/users", {
       method: 'POST',
       headers: new Headers({
         'Content-Type': 'application/json',
