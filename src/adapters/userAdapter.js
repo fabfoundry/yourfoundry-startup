4f78244cebf1b8159878b012c2269ace5b787f6f
Peter <ppapadim12@gmail.com>
2018-01-20 12:56:42 -0500

    console with add/delete project functionality

--- src/adapters/userAdapter.js
+++ src/adapters/userAdapter.js
@@ -1,8 +1,8 @@
 class UserAdapter {
 
   static create(credentials) {
-    // const request = new Request("https://stagingapi.yourfoundry.com/api/v1/users", {
-    const request = new Request("http://localhost:3000/api/v1/users", {
+    const request = new Request("https://stagingapi.yourfoundry.com/api/v1/users", {
+    // const request = new Request("http://localhost:3000/api/v1/users", {
       method: 'POST',
       headers: new Headers({
         'Content-Type': 'application/json',
@@ -18,8 +18,8 @@ class UserAdapter {
 
   static show(requestedData) {
     const authorizationToken = "Bearer " + sessionStorage.jwt
-    // const requestUrl = "https://stagingapi.yourfoundry.com/api/v1/users/0?q=" + requestedData
-    const requestUrl = "http://localhost:3000/api/v1/users/0?q=" + requestedData
+    const requestUrl = "https://stagingapi.yourfoundry.com/api/v1/users/0?q=" + requestedData
+    // const requestUrl = "http://localhost:3000/api/v1/users/0?q=" + requestedData
     const request = new Request(requestUrl, {
       method: 'GET',
       headers: new Headers({
